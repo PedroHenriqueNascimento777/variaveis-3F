fc76ee2de35bb638fe016418c72a54052761ccb3
PedrinXx777 <127054810+PedroHenriqueNascimento777@users.noreply.github.com>
2024-09-09 23:03:32 +0000

    09/09/2024

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const anoNascimento = 2006;
 let anoAtual = 2024;
 const nomeCompleto = "Pedro Henrique Nascimento";
 
-console.log("Meu nome é " + nomeCompleto + ", nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
+console.log(
+  "Meu nome é " +
+    nomeCompleto +
+    ", nascido no ano de " +
+    anoNascimento +
+    " e no ano atual " +
+    anoAtual +
+    " completo " +
+    idade +
+    " anos",
+);
 
 anoAtual = anoAtual + 1;
 console.log(anoAtual);
@@ -15,17 +25,28 @@ console.log(idade);
 
 const listaDeDestinos = new Array(
   "Atenas",
-  "Oslo",
-  "Paris",
-  "Nova Yorke",
-  "Cancun"
+  " Oslo",
+  " Paris",
+  " Nova Yorke",
+  " Cancun",
 );
 
 console.log(listaDeDestinos);
 console.log(listaDeDestinos[3]);
-listaDeDestinos.push("Coronel Vivida");
-listaDeDestinos.push("Guarulhos");
-listaDeDestinos.push("Madrid");
-listaDeDestinos.push("Wasshington DC");
+listaDeDestinos.push(" Coronel Vivida");
+listaDeDestinos.push(" Guarulhos");
+listaDeDestinos.push(" Madrid");
+listaDeDestinos.push(" Wasshington DC");
 
 console.log(listaDeDestinos);
+
+const idadeComprador = 17;
+
+if (idadeComprador >= 18) {
+  console.log("Comprador maior de idade");
+  console.log("Destinos disponiveis: " + listaDeDestinos);
+} else {
+
+  console.log("Comprador menor de idade");
+  console.log("Infelizmente não podemos vender para menores de idade");
+}
